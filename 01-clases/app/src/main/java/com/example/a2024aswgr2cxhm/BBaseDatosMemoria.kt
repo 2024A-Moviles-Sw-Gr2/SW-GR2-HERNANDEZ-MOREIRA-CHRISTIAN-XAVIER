fc832c79b5b62c441db723681503b65c6bdc6f65
ESCRITORIO VIRTUAL 28 <escritorio.virtual28@epn.edu.ec>
2024-06-14 11:01:48 -0500

    Context Menu class

---
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/BBaseDatosMemoria.kt
@@ -0,0 +1,24 @@
+package com.example.a2024aswgr2cxhm
+
+class BBaseDatosMemoria {
+    companion object {
+        val arregloBEntrenador = arrayListOf<BEntrenador>()
+
+        init {
+            arregloBEntrenador
+                .add(
+                    BEntrenador(1, "Adrian", "a@a.com")
+                )
+
+            arregloBEntrenador
+                .add(
+                    BEntrenador(2, "Vicente", "b@b.com")
+                )
+
+            arregloBEntrenador
+                .add(
+                    BEntrenador(3, "Carolina", "c@c.com")
+                )
+        }
+    }
+}
--- 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/BListView.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/BListView.kt
@@ -2,10 +2,87 @@ package com.example.a2024aswgr2cxhm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import com.google.android.material.snackbar.Snackbar
 
 class BListView : AppCompatActivity() {
+    val arreglo = BBaseDatosMemoria.arregloBEntrenador
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this, //Contexto
+        android.R.layout.simple_list_item_1, //Layout XML a usar
+        arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged() //Actualiza UI (muy importante)
+        val botonAnadirListView = findViewById<Button>(
+            R.id.btn_anadir_list_view
+        )
+        botonAnadirListView.setOnClickListener {
+            anadirEntrenador(adaptador)
+        }
+        registerForContextMenu(listView) //Nueva línea
+    } //Termina OnCreate
+
+    var posicionItemSeleccionado = -1
+    override fun onCreateContextMenu (
+        menu: ContextMenu,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ){
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //Llenamos opciones del menu
+        var inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        //Obtener id
+        val info =menuInfo as AdapterView.AdapterContextMenuInfo
+        val posicion = info.position
+        posicionItemSeleccionado = posicion
+    }
+
+    override fun onContextItemSelected(
+        item: MenuItem
+    ): Boolean {
+        return when (item.itemId){
+            R.id.mi_editar -> {
+                mostrarSanckbar(
+                    "Editar $posicionItemSeleccionado")
+                return true
+            }
+            R.id.mi_eliminar -> {
+                mostrarSanckbar(
+                    "Eliminar $posicionItemSeleccionado")
+                abrirDialogo() //Nueva línea
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    fun abrirDialogo(){}
+    fun anadirEntrenador (adaptador: ArrayAdapter<BEntrenador>
+    ){
+        arreglo.add(
+            BEntrenador(4, "Wendy", "d@d.com")
+        )
+        adaptador.notifyDataSetChanged()
+    }
+
+    fun mostrarSanckbar (texto:String) {
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            texto,
+        Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
     }
 }
