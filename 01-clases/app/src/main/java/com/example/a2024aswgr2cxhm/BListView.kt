e3fa0a0f34c15aaf3ef3eb88964b7a5cd418a906
ESCRITORIO VIRTUAL 28 <escritorio.virtual28@epn.edu.ec>
2024-06-20 08:35:07 -0500

    App Released

--- 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/BListView.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/BListView.kt
@@ -1,5 +1,6 @@
 package com.example.a2024aswgr2cxhm
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
@@ -9,6 +10,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 
 class BListView : AppCompatActivity() {
@@ -68,7 +70,35 @@ class BListView : AppCompatActivity() {
         }
     }
 
-    fun abrirDialogo(){}
+    fun abrirDialogo(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Desea Eliminar?")
+        builder.setPositiveButton(
+            "Aceptar",
+            DialogInterface.OnClickListener{
+                dialog, which ->
+            mostrarSanckbar("Acepto $which")
+            }
+        )
+        builder.setNegativeButton("Cancelar", null)
+        val opciones = resources.getStringArray(
+            R.array.string_array_opciones
+        )
+        val seleccionPrevia = booleanArrayOf(
+            true, // Lunes,
+            false, //Martes,
+            false, //Miércoles
+        )
+
+        builder.setMultiChoiceItems(
+            opciones,
+            seleccionPrevia
+        ) { dialog, which, isChecked ->
+            mostrarSanckbar("Item: $which")
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
     fun anadirEntrenador (adaptador: ArrayAdapter<BEntrenador>
     ){
         arreglo.add(
@@ -79,7 +109,7 @@ class BListView : AppCompatActivity() {
 
     fun mostrarSanckbar (texto:String) {
         val snack = Snackbar.make(
-            findViewById(R.id.cl_ciclo_vida),
+            findViewById(R.id.cl_blist_view),
             texto,
         Snackbar.LENGTH_INDEFINITE
         )
