06a2331b5d549c81a34534ff60b25cc8706d5eb4
ESCRITORIO VIRTUAL 28 <escritorio.virtual28@epn.edu.ec>
2024-07-04 08:59:09 -0500

    Intents (Explícito e implícito)

--- 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.a2024aswgr2cxhm
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    fun mostrarSnackBar(texto:String) {
+        val snack = Snackbar.make(
+            findViewById(R.id.id_layout_main),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    // Lógica Negocio
+                    val data = result.data
+                    mostrarSnackBar(
+                        "${data?.getStringExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+
+    val callbackContenidoIntentImplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    // Lógica Negocio
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri, null, null, null, null, null
+                        )
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackBar("Telefono $telefono")
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,7 +80,36 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener{
                 irActividad(BListView::class.java)
             }
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito
+        )
+        botonIntentImplicito
+            .setOnClickListener{
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackContenidoIntentImplicito.launch(intentConRespuesta)
+            }
 
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito
+            .setOnClickListener{
+                val intentExplicito = Intent(
+                    this,
+                    CIntentExplicitoParametros::class.java
+                )
+                intentExplicito.putExtra("nombre", "Christian")
+                intentExplicito.putExtra("apellido", "Hernández")
+                intentExplicito.putExtra("edad", 22)
+                intentExplicito.putExtra(
+                    "entrenador",
+                    BEntrenador(10, "Jose", "Perez")
+                )
+                callbackContenidoIntentImplicito.launch(intentExplicito)
+            }
     }
 
     fun irActividad (
