bc33d6d561283365e6c0e2796378831ffedcd4a3
ESCRITORIO VIRTUAL 28 <escritorio.virtual28@epn.edu.ec>
2024-07-19 09:48:15 -0500

    Recycler Views

--- 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2cxhm/MainActivity.kt
@@ -120,6 +120,10 @@ class MainActivity : AppCompatActivity() {
         botonSqLite.setOnClickListener {
             irActividad(ECrudEntrenador::class.java)
         }
+        val botonRView = findViewById<Button>(R.id.btn_recycler_view)
+        botonRView.setOnClickListener {
+            irActividad(FRecyclerView::class.java)
+        }
     }
 
     fun irActividad (
